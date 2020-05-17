fc70db8414425808e7a979c3a0ca3dadabf3cb26
vishal <vishalparekh130@gmail.com>
2020-05-17 15:31:13 +0530

    Edit User Reg.js

--- React_Views/views/src/components/userReg.js
+++ React_Views/views/src/components/userReg.js
@@ -65,6 +65,7 @@ export class userReg extends Component {
                     </div>
                     <ul className="nav navbar-nav">
                         <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
                     </ul>
                     <ul className="nav navbar-nav navbar-right">
                         <li><a href="#"><span className="glyphicon glyphicon-user"></span>Sign Up</a></li>
