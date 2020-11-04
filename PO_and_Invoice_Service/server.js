6eed2fc6f94b5245ff770e156ac69ad0fba1ac54
Vishal Parekh <vishalparekh130@gmail.com>
2020-11-04 12:08:56 +0530

    Ready For Heroku

--- PO_and_Invoice_Service/server.js
+++ PO_and_Invoice_Service/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
-const port = 3002;
+const port = process.env.PORT || 3002;
 const server = http.createServer(app);
 server.listen(port);
